4d7f3845045a148156a201dd3cc79559796c46b3
Aubrey Guishard <aubrey.guishard@gmail.com>
2022-12-20 15:50:56 -0500

    I was able to successfully run the functions and added the functions to the run_game function of which it displayed the fight and the successor

--- Battlefield.py
+++ Battlefield.py
@@ -6,20 +6,23 @@ class Battlefield:
     def __init__(self):
         self.dinosaur = Dinosaur("Rexy", 250, 1000)
         self.robot = Robot("Optimus", 1000)
-        self.weapon = Weapon("Sword", 100)
+        self.active_weapon = Weapon("Sword", 100)
     def run_game(self):
-        pass
+        battlefield_one = Battlefield()
+        battlefield_one.display_welcome()
+        battlefield_one.battle_phase()
+        battlefield_one.display_winner()
 
     def display_welcome(self):
          print("\nWelcome to the most epic duel of the century!\nThere will only be ONE victor, so let us begin!\n")
 
     def battle_phase(self):
          while self.dinosaur.dinosaur_still_alive() and self.robot.robot_still_alive():
-            self.dinosaur.dinosaur_attack(self.robot)
-            print(f"{self.dinosaur.name} attacked {self.robot.name}, now {self.robot.name} health is at {self.robot.health}")
-
             self.robot.robot_attack(self.dinosaur)
             print(f"{self.robot.name} attacked {self.dinosaur.name}, now {self.dinosaur.name} health is at {self.dinosaur.health}")
+
+            self.dinosaur.dinosaur_attack(self.robot)
+            print(f"{self.dinosaur.name} attacked {self.robot.name}, now {self.robot.name} health is at {self.robot.health}")
            
            
 
--- main.py
+++ main.py
@@ -2,5 +2,4 @@ from battlefield import Battlefield
 
 
 battlefield_one = Battlefield()
-battlefield_one.display_welcome()
-battlefield_one.battle_phase()
+battlefield_one.run_game()
